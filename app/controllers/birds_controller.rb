d8e63e57f0d4fdf1b700255a8a02e6fdb48683f9
Rodgers Ogada <ogadarodgers@gmail.com>
2023-03-14 11:22:21 +0300

    Done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -42,6 +42,18 @@ class BirdsController < ApplicationController
     else
       render json: { error: "Bird not found" }, status: :not_found
     end
+
+    # DESTROY /birds/:id
+    def destroy
+      bird = Bird.find_by(id: params[:id])
+      if bird
+        bird.destroy
+        head :no_content
+      else
+        render json: { error: "Bird not found" }, status: :not_found
+      end
+    end
+
   end
 
   private
